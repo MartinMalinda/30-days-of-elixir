f48770d873be7215a9da6cb1260a7d6c893deecb
martinmalinda <malindacz@gmail.com>
2016-08-05 21:54:49 +0200

    add fibonacci algorithm that does not use Enum.at so it does not traverse the array all the time

--- 07-fibonacci.exs
+++ 07-fibonacci.exs
@@ -1,81 +1,20 @@
-defmodule Fib do
-  @moduledoc """
-  Fibonacci Sequence function.
-  Please note, I wrote this purely from memory --
-  I mean, I'm sure there's a more concise way to build this algorithm. :-)
-  """
-
-  @seed [0, 1]
-
-  # named functions can have different arities, whereas anonymous functions cannot
-  # also, anonymous functions cannot call themselves recursively :-(
-
-  def fib(n) when n < 2 do
-    Enum.take @seed, n
+defmodule Fibonacci do
+  def fib(1), do: [0]
+  def fib(2), do: [0, 1]
+  def fib(list_length) do
+    fib(3, [0, 1], 0, 1, list_length)
   end
-
-  def fib(n) when n >= 2 do
-    fib(@seed, n - 2)
+  def fib(_, list, _, _, list_length) when length(list) == list_length do
+    list
   end
-
-  def fib(acc, 0), do: acc
-
-  def fib(acc, n) do
-    fib(acc ++ [Enum.at(acc, -2) + Enum.at(acc, -1)], n - 1)
+  def fib(position, list, older, old, list_length) when position > 2 and is_list(list) do
+    IO.puts older
+    IO.puts old
+    latest = (old + older)
+    fib(position + 1, list ++ [latest], old, latest, list_length)
   end
 end
 
-defmodule Fib2 do
-  @moduledoc """
-  Fibonacci Sequence function.
-  This is my attempt to be more efficient by building the list
-  backwards (and then reversing at the end).
-  """
-
-  @seed [1, 0]
-
-  def fib2(n) when n < 2 do
-    Enum.reverse(@seed) |> Enum.take(n)
-  end
-
-  def fib2(n) when n >= 2 do
-    fib2(@seed, n - 2)
-  end
-
-  def fib2(acc, 0), do: Enum.reverse(acc)
-
-  def fib2([first, second | _] = lst, n) do
-    fib2([first + second | lst], n - 1)
-  end
-end
-
-ExUnit.start
-
-defmodule RecursionTest do
-  use ExUnit.Case
-
-  import Fib
-
-  test "fibonacci" do
-    assert fib(0) == []
-    assert fib(1) == [0]
-    assert fib(2) == [0, 1]
-    assert fib(10) == [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
-  end
-
-  import Fib2
-
-  test "fibonacci 2" do
-    assert fib2(0) == []
-    assert fib2(1) == [0]
-    assert fib2(2) == [0, 1]
-    assert fib2(10) == [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
-  end
-
-  test "benchmark" do
-    {microsecs, _} = :timer.tc fn -> fib(1000) end
-    IO.puts "fib() took #{microsecs} microsecs"     # 7118 microsecs
-    {microsecs, _} = :timer.tc fn -> fib2(1000) end
-    IO.puts "fib2() took #{microsecs} microsecs"    # 90 microsecs
-  end
-end
+Fibonacci.fib(4)
+|> List.to_string
+|> IO.puts
